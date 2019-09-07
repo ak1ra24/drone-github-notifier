74cce955b09155e40b45f9e2fcf622c06cf4b814
ak1ra24 <marug4580@gmail.com>
2019-09-08 02:47:35 +0900

    forth

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -9,8 +10,23 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
+var (
+	owner = flag.String("owner", "", "github owner")
+	repo  = flag.String("repo", "", "github repo")
+)
+
 func main() {
-	config := githubapi.NewClient("ak1ra24", "github-api-go")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `
+Usage of %s:
+   %s [OPTIONS] ARGS...
+Options\n`, os.Args[0], os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	config := githubapi.NewClient(*owner, *repo)
 	fmt.Println(terminal.IsTerminal(0))
 	if terminal.IsTerminal(0) {
 		fmt.Println("パイプ無し(FD値0)")
