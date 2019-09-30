5417020c17e0beacc59ad95fa9b7fe9449e2b97d
ak1ra24 <marug4580@gmail.com>
2019-09-30 21:31:54 +0900

    prcomment depends on build_event 2

--- main.go
+++ main.go
@@ -68,16 +68,16 @@ func main() {
 		fmt.Errorf("Not Support")
 	}
 
-	if ciservice.Event == "pull_request" {
-		pr := ciservice.PR
-		fmt.Println(pr)
+	pr := ciservice.PR
+	fmt.Println(pr)
 
-		client := githubapi.NewClient(github_settings.Repository.Owner, github_settings.Repository.Repo, github_settings.Token, pr)
-		if terminal.IsTerminal(0) {
-			fmt.Println("パイプ無し")
-		} else {
-			b, _ := ioutil.ReadAll(os.Stdin)
-			fmt.Println("パイプで渡された内容:", string(b))
+	client := githubapi.NewClient(github_settings.Repository.Owner, github_settings.Repository.Repo, github_settings.Token, pr)
+	if terminal.IsTerminal(0) {
+		fmt.Println("パイプ無し")
+	} else {
+		b, _ := ioutil.ReadAll(os.Stdin)
+		fmt.Println("パイプで渡された内容:", string(b))
+		if ciservice.Event == "pull_request" {
 			if err := client.PRComment(string(b)); err != nil {
 				panic(err)
 			}
